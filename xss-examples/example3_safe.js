f4a30e0fe2dc89c5b283501043dff08c13bf333e
Joel J <joelsj2305@gmail.com>
2025-12-13 19:15:02 -0500

    some sast fixes

--- xss-examples/example3_safe.js
+++ xss-examples/example3_safe.js
@@ -1,11 +1,16 @@
-const escapeHtml = s => s
-  .replace(/&/g, '&amp;')
-  .replace(/</g, '&lt;')
-  .replace(/>/g, '&gt;')
-  .replace(/"/g, '&quot;')
-  .replace(/'/g, '&#x27;');
+import { JSDOM } from 'jsdom';
+import createDOMPurify from 'dompurify';
+
+const window = new JSDOM('').window;
+const DOMPurify = createDOMPurify(window);
+
 
 app.get('/search', (req, res) => {
-  const q = escapeHtml(req.query.q || '');
-  res.send(`<h1>Results for ${q}</h1>`);
+  const q = req.query.q || '';
+
+  const vuln = '<h1>Results for ${q}</h1>';
+
+  const safe = DOMPurify.sanitize(vuln);
+
+  res.send(safe);
 });
--- xss-examples/example3_vuln.js
+++ xss-examples/example3_vuln.js
@@ -1,4 +1,16 @@
+import { JSDOM } from 'jsdom';
+import createDOMPurify from 'dompurify';
+
+const window = new JSDOM('').window;
+const DOMPurify = createDOMPurify(window);
+
+
 app.get('/search', (req, res) => {
   const q = req.query.q || '';
-  res.send(`<h1>Results for ${q}</h1>`);
+
+  const vuln = '<h1>Results for ${q}</h1>';
+
+  const safe = DOMPurify.sanitize(vuln);
+
+  res.send(safe);
 });
